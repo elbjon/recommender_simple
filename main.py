8eb75158b96c975100d28ede7ada75e3e9481a33
Jonas Elbers <143721070+elbjon@users.noreply.github.com>
2023-11-09 17:48:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,3 +7,12 @@ df = pd.read_csv('popularity_ranking.csv')
 
 
 st.write(df.head(20))
+
+
+option = st.selectbox(
+    'Choose a movie!',
+    df['title'].head(50))
+
+
+
+st.write('You selected:', option)
