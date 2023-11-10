4376b679abbbe28ef50baaf9c1241a41f9cf165c
Jonas Elbers <143721070+elbjon@users.noreply.github.com>
2023-11-10 18:40:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -54,31 +54,29 @@ abc = st.slider(
 
 n = abc
 
-#def myfunction(movieId, n):
+# Button that contains the functionality to give out item based recommendations
 if st.button("Give me recommendations", type="primary"):
     lovely_bones_isbn = str(int(movieId))
-    #st.write(lovely_bones_isbn, n)
-    #st.write(movies_cosines_matrix)
-#    # Create a DataFrame using the values from 'books_cosines_matrix' for the 'lovely_bones_isbn' book.
+
+    # Create a DataFrame using the values from 'books_cosines_matrix' for the 'lovely_bones_isbn' book.
     lovely_bones_cosines_df = pd.DataFrame(movies_cosines_matrix.loc[:,str(n)])
     
 #
 #    # Rename the column 'lovely_bones_isbn' to 'lovely_bones_cosine'
     lovely_bones_cosines_df = lovely_bones_cosines_df.rename(columns={lovely_bones_isbn: 'lovely_bones_cosine'})
-#
+
 #   # Remove the row with the index 'lovely_bones_isbn'
     lovely_bones_cosines_df = lovely_bones_cosines_df[lovely_bones_cosines_df.index != lovely_bones_isbn]
-    #st.write(lovely_bones_isbn, n,lovely_bones_cosines_df)
+
 #    # Sort the 'lovely_bones_cosines_df' by the column 'lovely_bones_cosine' column in descending order.
     lovely_bones_cosines_df = lovely_bones_cosines_df.sort_values(by=str(int(n)), ascending=False)
-    #st.write('67', lovely_bones_isbn, n,lovely_bones_cosines_df)
-    #st.write('68', user_movies_matrix)
+
 #    # Find out the number of users rated both The Lovely Bones and the other book
     no_of_users_rated_both_books = [sum((user_movies_matrix[str(int(lovely_bones_isbn))] > 0) & (user_movies_matrix[str(int(isbn))] > 0)) for isbn in lovely_bones_cosines_df.index]
-    #st.write('71', no_of_users_rated_both_books)
+
 #    # Create a column for the number of users who rated The Lovely Bones and the other book
     lovely_bones_cosines_df['users_who_rated_both_books'] = no_of_users_rated_both_books
-#
+
 #    # Remove recommendations that have less than 10 users who rated both books.
 #    lovely_bones_cosines_df = lovely_bones_cosines_df[lovely_bones_cosines_df["users_who_rated_both_books"] > 5]
 
