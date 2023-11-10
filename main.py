89540554297dd0568ae3c05642cc5c05d0bc2ddf
Jonas Elbers <143721070+elbjon@users.noreply.github.com>
2023-11-10 11:14:16 +0100

    Update main.py

--- main.py
+++ main.py
@@ -83,7 +83,7 @@ if st.button("Give me recommendations", type="primary"):
               .merge(df.drop_duplicates(subset='movieId'),
                                          on='movieId',
                                          how='left'))
-    st.write(my_top_10)
+    st.write(my_top_10[['title','mean']])
         
 
 
