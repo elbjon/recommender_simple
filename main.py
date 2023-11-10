7de39ab6d831361eb3fca79513c993988024724d
Jonas Elbers <143721070+elbjon@users.noreply.github.com>
2023-11-10 12:40:33 +0100

    Update main.py

--- main.py
+++ main.py
@@ -100,24 +100,6 @@ if st.button("Give me recommendations", type="primary"):
 #    st.write(f'You selected: {n} recommendations to {option}')
 #    st.write(myfunction(5, 2))
 
-import streamlit.components.v1 as components
-
-def main():
-
-    imageCarouselComponent = components.declare_component("image-carousel-component", path="frontend/public")
-
-    imageUrls = [
-        "https://images.unsplash.com/photo-1522093007474-d86e9bf7ba6f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=764&q=80",
-        "https://images.unsplash.com/photo-1610016302534-6f67f1c968d8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1075&q=80",
-        "https://images.unsplash.com/photo-1516550893923-42d28e5677af?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=872&q=80",
-    ]
-    selectedImageUrl = imageCarouselComponent(imageUrls=imageUrls, height=200)
-
-    if selectedImageUrl is not None:
-        st.image(selectedImageUrl)
-
-if __name__ == "__main__":
-    main()
 
 st.markdown('''
     :blue[Disclaimer: This application uses TMDB and the TMDB APIs but is not endorsed, certified, or otherwise approved by TMDB.] ''')
