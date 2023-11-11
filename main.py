dad34c6ad4692098a20a9485f5b1ed3fb31cf113
Jonas Elbers <143721070+elbjon@users.noreply.github.com>
2023-11-11 04:22:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -69,7 +69,7 @@ if st.button("Give me recommendations", type="primary"):
     lovely_bones_cosines_df = lovely_bones_cosines_df[lovely_bones_cosines_df.index != lovely_bones_isbn]
     st.write(lovely_bones_cosines_df.head(10))
     # Sort the 'lovely_bones_cosines_df' by the column 'lovely_bones_cosine' column in descending order.
-    lovely_bones_cosines_df = lovely_bones_cosines_df.sort_values(by=str(movieId)), ascending=False)
+    lovely_bones_cosines_df = lovely_bones_cosines_df.sort_values(by=str(int(movieId))), ascending=False)
 
     # Find out the number of users rated both The Lovely Bones and the other book
     no_of_users_rated_both_books = [sum((user_movies_matrix[str(int(lovely_bones_isbn))] > 0) & (user_movies_matrix[str(int(isbn))] > 0)) for isbn in lovely_bones_cosines_df.index]
